3de1166c55e392b18e8e8a7096fdb9c803f82ce2
ciokos <wciokos@gmail.com>
2018-10-14 21:27:08 +0200

    more solves

--- bmi1_english.R
+++ bmi1_english.R
@@ -45,6 +45,8 @@ tail(D)
 summary(D)
 ## Another type of summary of the dataset
 str(D)
+## Check for missing values
+sum(is.na.data.frame(D))
 
 ###########################################################################
 ## Calculate BMI scores
@@ -57,7 +59,12 @@ D$bmi <- D$weight/(D$height/100)^2
 
 ## Histogram describing the empirical density of the BMI scores
 ## (histogram of the BMI scores normalized to have an area of 1)
-hist(D$bmi, xlab="BMI", prob=TRUE)
+
+hist(D$bmi, main = "Density Histogram of BMI", xlim = c(15,40),xlab="BMI", prob=TRUE,
+     las=1, 
+     col = "green")
+abline(v=median(D$bmi),col="red")
+abline(v=mean(D$bmi),col="blue")
 
 ###########################################################################
 ## Taking subsets of the data using 'subset'
@@ -74,6 +81,18 @@ Dmale <- subset(D, gender == 1)
 hist(Dfemale$bmi, xlab="BMI (female)", prob=TRUE)
 hist(Dmale$bmi, xlab="BMI (male)", prob=TRUE)
 
+hist(Dfemale$bmi, main = "Density Histogram of BMI for Females", xlim = c(15,40),xlab="BMI", prob=TRUE,
+     las=1, 
+     col = "green")
+abline(v=median(Dfemale$bmi),col="red")
+abline(v=mean(Dfemale$bmi),col="blue")
+
+hist(Dmale$bmi, main = "Density Histogram of BMI for Males", xlim = c(15,40),xlab="BMI", prob=TRUE,
+     las=1, 
+     col = "green")
+abline(v=median(Dmale$bmi),col="red")
+abline(v=mean(Dmale$bmi),col="blue")
+
 ###########################################################################
 ## Box plot
 
@@ -84,17 +103,26 @@ boxplot(Dfemale$bmi, Dmale$bmi, names=c("Female", "Male"),
 ###########################################################################
 ## Summary statistics for BMI
 
-## Total number of observations
-## (doesn't include missing values if there are any)
-sum(!is.na(D$bmi))
-## Sample mean (both genders combined)
-mean(D$bmi, na.rm=TRUE)
-## Sample variance (both genders combined)
-var(D$bmi, na.rm=TRUE)
-## etc.
-##
-## The argument 'na.rm=TRUE' ensures that the statistic is
-## computed even in cases where there are missing values.
+Tbl <- data.frame()
+Tbl[1, "Number of obs."] <- sum(!is.na(D$bmi))
+Tbl[1, "Sample mean"] <- mean(D$bmi)
+Tbl[1, "Sample Variance"] <- var(D$bmi)
+Tbl[1, "Sample st. dev."] <- sd(D$bmi)
+Tbl[1, "Lower quartile"] <- quantile(D$bmi, 0.25)
+Tbl[1, "Median"] <- median(D$bmi)
+Tbl[1, "Upper quartile"] <- quantile(D$bmi, 0.75)
+for(i in 0:1){
+  Tbl[i+2, "Number of obs."] <- sum(!is.na(D$bmi[D$gender == i]))
+  Tbl[i+2, "Sample mean"] <- mean(D$bmi[D$gender == i])
+  Tbl[i+2, "Sample Variance"] <- var(D$bmi[D$gender == i]) 
+  Tbl[i+2, "Sample st. dev."] <- sd(D$bmi[D$gender == i]) 
+  Tbl[i+2, "Lower quartile"] <- quantile(D$bmi[D$gender == i], 0.25)
+  Tbl[i+2, "Median"] <- median(D$bmi[D$gender == i])
+  Tbl[i+2, "Upper quartile"] <- quantile(D$bmi[D$gender == i], 0.75)
+}
+
+row.names(Tbl) <- c("Everyone", "Women","Men")
+xtable(Tbl, align = "p{1.5cm}p{1.5cm}p{1.5cm}p{1.5cm}p{1.5cm}p{1.5cm}p{1.5cm}p{1.5cm}")
 
 ###########################################################################
 ## qq-plot for model validation
@@ -105,10 +133,34 @@ D$logbmi <- log(D$bmi)
 qqnorm(D$logbmi)
 qqline(D$logbmi)
 
+## histogram of log-transformed BMI
+hist(D$logbmi, main = "Density Histogram of log transformed BMI", xlab="BMI", prob=TRUE,
+     las=1, 
+     col = "green")
+
+mean(D$logbmi)
+sd(D$logbmi)
+length(D$logbmi)
+##student-t
+qt(p=0.975, df=length(D$logbmi)-1)
+res <- t.test(D$logbmi, conf.level = 0.95)
+
+##range of median BMI
+exp(res[['conf.int']][1])
+exp(res[['conf.int']][2])
+
 ###########################################################################
 ## One-sample t-test
-
+mean <- mean(D$logbm)
+u0 <- log(25)
+s = sd(D$logbm)
+n = length(D$logbm)
+tobs <- (mean-u0)/(s/sqrt(n))
+tobs
+pvalue <- 2*(1-pt(abs(tobs), df=n-1))
+pvalue
 ## Testing hypothesis mu=log(25) for log-transformed BMI
+
 t.test(D$logbmi, mu=log(25))
 
 ###########################################################################
